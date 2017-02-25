d357ff2ff4bb0b5d4d52aec09aef156a65e2f0e1
Mariusz Szyma <m.szyma92@gmail.com>
2017-02-25 23:02:34 +0100

    Working API with JSON html implementation :D

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,8 @@ var url = "http://www.filltext.com/?rows=30&fname={firstName}&lname={lastName}&p
 $(document).ready(function() {
   $('.quote-button').on('click', function() {
     $.getJSON(url, function(data) {
-      console.log(data[0].fname);
-
+      $('#placeQuote').html(data[0].words);
+      $('.footer').html(data[0].fname + " " + data[0].lname);
     });
   });
-
 });
