a2d5ca00602c3cd143ef11393b97a6386b70691e
Mariusz Szyma <m.szyma92@gmail.com>
2017-02-28 14:13:27 +0100

    Tweet each time the right quote

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,18 @@ $(document).ready(function() {
         $.getJSON(url, function(data) {
             $('#placeQuote').html(data[0].words);
             $('.footer').html(data[0].fname + " " + data[0].lname);
+            $('#placeQuote').html(data[0].words);
         });
     });
 });
+
+
+function tweetIt () {
+  var phrase = document.getElementById('placeQuote').innerText;
+  var tweetUrl = 'https://twitter.com/share?text=' +
+    encodeURIComponent(phrase) +
+    '.' +
+    '#quotes';
+
+  window.open(tweetUrl);
+}
