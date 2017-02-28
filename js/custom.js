8c28ee40eea423928b84485a224327ff10cb3273
Mariusz Szyma <m.szyma92@gmail.com>
2017-02-28 11:17:45 +0100

    Showing quotes on pageload

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,13 @@
 var url = "http://www.filltext.com/?rows=30&fname={firstName}&lname={lastName}&pretty=true&words={lorem|15}";
 
 $(document).ready(function() {
-  $('.quote-button').on('click', function() {
     $.getJSON(url, function(data) {
-      $('#placeQuote').html(data[0].words);
-      $('.footer').html(data[0].fname + " " + data[0].lname);
+        $('#placeQuote').html(data[0].words);
     });
-  });
+    $('.quote-button').on('click', function() {
+        $.getJSON(url, function(data) {
+            $('#placeQuote').html(data[0].words);
+            $('.footer').html(data[0].fname + " " + data[0].lname);
+        });
+    });
 });
