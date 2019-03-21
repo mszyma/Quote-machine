7df631d9c15e3623cd1e56799034f7f7cc998eff
Mariusz Szyma <m.szyma92@gmail.com>
2019-03-22 00:00:26 +0100

    Update author during first load

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,7 @@ var url = "https://quotes.stormconsultancy.co.uk/random.json";
 $(document).ready(function() {
     $.getJSON(url, function(data) {
         $('#placeQuote').html(data.quote);
+        $('.footer').html(data.author);
     });
     $('.quote-button').on('click', function() {
         $.getJSON(url, function(data) {
