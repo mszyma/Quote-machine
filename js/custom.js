8392f1c75b77fb2b668e786e8d7cfda7e3f095e5
Mariusz Szyma <m.szyma92@gmail.com>
2019-03-21 23:44:42 +0100

    Changed quote API to something related to software

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,14 @@
-var url = "https://www.filltext.com/?rows=30&fname={firstName}&lname={lastName}&pretty=true&words={lorem|15}";
+var url = "http://quotes.stormconsultancy.co.uk/random.json";
 
 $(document).ready(function() {
     $.getJSON(url, function(data) {
-        $('#placeQuote').html(data[0].words);
+        $('#placeQuote').html(data.quote);
     });
     $('.quote-button').on('click', function() {
         $.getJSON(url, function(data) {
-            $('#placeQuote').html(data[0].words);
-            $('.footer').html(data[0].fname + " " + data[0].lname);
-            $('#placeQuote').html(data[0].words);
+            $('#placeQuote').html(data.quote);
+            $('.footer').html(data.author);
+            $('#placeQuote').html(data.quote);
         });
     });
 });
